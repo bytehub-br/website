7e3c416e91cdbe1bcd267466ea39ed835683bce6
Matheus Mattana <matheusmattana@hotmail.com>
2019-08-07 11:59:51 -0300

    new main image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
-import { LogoComponent } from './logo/logo.component';
+import { LogoComponent } from './home/logo/logo.component';
 import { PresentationTitleComponent } from './presentation-title/presentation-title.component';
 import { MainIllustrationComponent } from './main-illustration/main-illustration.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeModule } from './home/home.module';
 import { MaterialModule } from './material/material.module';
-import { PresentationTextComponent } from './main-content/presentation-text/presentation-text.component';
+import { PresentationTextComponent } from './home/main-content/presentation-text/presentation-text.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LogoComponent,
-    PresentationTitleComponent,
-    MainIllustrationComponent,
-    PresentationTextComponent
+  
   ],
   imports: [
     BrowserModule,
